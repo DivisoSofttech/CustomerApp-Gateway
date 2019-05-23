fdf81dc28809e8064fcc2fd932f0ae776c80c98a
mayabytatech <maya.k.k@lxisoft.com>
2019-05-23 23:31:54 +0530

    log factored

--- src/main/java/com/diviso/graeshoppe/web/rest/QueryResource.java
+++ src/main/java/com/diviso/graeshoppe/web/rest/QueryResource.java
@@ -330,11 +330,11 @@ public class QueryResource {
 		List<Customer> customerList = queryService.findAllCustomersWithoutSearch(pageable).getContent();
 
 		for (Customer c : customerList) {
-        
+        log.info(">>>>>>>>>>>>>>>>>>> customer:   "+c+"   >>>>>>>>>>>>>>>>");
 			UserRating rating = queryService.findRatingByStoreIdAndCustomerName(storeId, c.getName());
-
+			log.info(">>>>>>>>>>>>>>>>>>> rating:  "+rating+"   >>>>>>>>>>>>>>>>");
 			Review review = queryService.findReviewByStoreIdAndCustomerName(storeId, c.getName());
-			
+			log.info(">>>>>>>>>>>>>>>>>>> review:  "+review+"   >>>>>>>>>>>>>>>>");
 			if (rating != null) {
 				
 				RatingReview ratingReview = new RatingReview();
@@ -344,7 +344,9 @@ public class QueryResource {
 				if(review!=null){
 				ratingReview.setReview(reviewResourceApi.modelToDtoUsingPOST(review).getBody());
 				}
+				log.info(">>>>>>>>>>>>>>>>>>> ratingReview:  "+ratingReview+"   >>>>>>>>>>>>>>>>");
 				listOfRatingreview.add(ratingReview);
+				log.info(">>>>>>>>>>>>>>>>>>> listOfRatingreview:  "+listOfRatingreview+"   >>>>>>>>>>>>>>>>");
 			}
 		}
 
