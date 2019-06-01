c0514af7c6d40d3750554c2f20f2ddcb4104235f
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-06-01 14:37:08 +0530

    refactored

--- src/main/java/com/diviso/graeshoppe/service/impl/QueryServiceImpl.java
+++ src/main/java/com/diviso/graeshoppe/service/impl/QueryServiceImpl.java
@@ -498,8 +498,8 @@ public class QueryServiceImpl implements QueryService {
 	@Override
 	public Page<Product> findProductByStoreIdAndCategoryName(String userId, String categoryName, Pageable pageable) {
 		SearchQuery searchQuery = new NativeSearchQueryBuilder()
-				.withQuery(QueryBuilders.boolQuery().must(QueryBuilders.termQuery("userId.keyword", "hotel"))
-						.must(QueryBuilders.matchQuery("categories.name.keyword", "arabian special")))
+				.withQuery(QueryBuilders.boolQuery()/*.must(QueryBuilders.termQuery("userId.keyword", "hotel"))*/
+						.must(QueryBuilders.matchQuery("categories.name", "arabian special")))
 				.build();
 		
 		Page<Product> p=	elasticsearchOperations.queryForPage(searchQuery, Product.class);
