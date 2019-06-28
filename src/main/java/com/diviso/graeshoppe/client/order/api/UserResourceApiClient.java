c817010b4f02c2f07c07bcef526965d9b2446434
mayabytatech <maya.k.k@lxisoft.com>
2019-06-28 13:53:02 +0530

    apiclient url changed

--- src/main/java/com/diviso/graeshoppe/client/order/api/UserResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/order/api/UserResourceApiClient.java
@@ -3,6 +3,6 @@ package com.diviso.graeshoppe.client.order.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.diviso.graeshoppe.client.order.OrderClientConfiguration;
 
-@FeignClient(name="${order.name:order}", url="${order.url:35.237.237.182:8088/}", configuration = OrderClientConfiguration.class)
+@FeignClient(name="${order.name:order}", url="${order.url:35.237.4.7:8088/}", configuration = OrderClientConfiguration.class)
 public interface UserResourceApiClient extends UserResourceApi {
 }
