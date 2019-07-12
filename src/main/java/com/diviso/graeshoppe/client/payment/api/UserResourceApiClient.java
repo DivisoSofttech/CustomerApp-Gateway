147a82375b339ed82abf844bc7b78defa88e8ed5
abilash-diviso <abilash.s@lxisoft.com>
2019-07-12 12:50:26 +0530

    payment razorpay order creation implemented

---
+++ src/main/java/com/diviso/graeshoppe/client/payment/api/UserResourceApiClient.java
@@ -0,0 +1,8 @@
+package com.diviso.graeshoppe.client.payment.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import com.diviso.graeshoppe.client.payment.RazorpayClientConfiguration;
+
+@FeignClient(name="${payment.name:payment}", url="${payment.url:localhost:9090/}", configuration = RazorpayClientConfiguration.class)
+public interface UserResourceApiClient extends UserResourceApi {
+}
