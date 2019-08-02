bb32facf5a8dbb9b9f5ab3e4c59143ced974b951
mayabytatech <maya.k.k@lxisoft.com>
2019-08-02 12:03:08 +0530

    url removed

--- src/main/java/com/diviso/graeshoppe/client/store/api/StoreResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/store/api/StoreResourceApiClient.java
@@ -5,6 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import com.diviso.graeshoppe.client.store.StoreClientConfiguration;
 
 
-@FeignClient(name="${store.name:store}", url="${store.url:34.74.142.114:8071/}", configuration = StoreClientConfiguration.class)
+@FeignClient(name="${store.name:store}", url="${store.url}", configuration = StoreClientConfiguration.class)
 public interface StoreResourceApiClient extends StoreResourceApi {
 }
--- src/main/java/com/diviso/graeshoppe/client/store/api/StoreSettingsResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/store/api/StoreSettingsResourceApiClient.java
@@ -3,6 +3,6 @@ package com.diviso.graeshoppe.client.store.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.diviso.graeshoppe.client.store.StoreClientConfiguration;
 
-@FeignClient(name="${store.name:store}", url="${store.url:34.74.142.114:8071/}", configuration = StoreClientConfiguration.class)
+@FeignClient(name="${store.name:store}", url="${store.url}", configuration = StoreClientConfiguration.class)
 public interface StoreSettingsResourceApiClient extends StoreSettingsResourceApi {
 }
