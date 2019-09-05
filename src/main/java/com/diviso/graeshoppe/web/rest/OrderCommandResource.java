c8c2bf3ce0e0e3ae8ed7f37dbc8ba2fd7769ffdc
sruthykc <sruthy.k.c@gmail.com>
2019-09-05 11:53:20 +0530

    Order Address Delete API

--- src/main/java/com/diviso/graeshoppe/web/rest/OrderCommandResource.java
+++ src/main/java/com/diviso/graeshoppe/web/rest/OrderCommandResource.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -203,4 +204,10 @@ public class OrderCommandResource {
 		return addressResourceApi.updateAddressUsingPUT(addressDTO);
 	}
 
+	@DeleteMapping("/addresses/{id}")
+	public ResponseEntity<Void> deleteAddress(@PathVariable Long id) {
+		return addressResourceApi.deleteAddressUsingDELETE(id);
+	}
+	
+	
 }
