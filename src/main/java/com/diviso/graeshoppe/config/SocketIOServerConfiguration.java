bec6047414878a51f477b343afbba06c242f4200
abilash-diviso <abilash.s@lxisoft.com>
2019-09-20 21:11:03 +0530

    socket configurations are changed

--- src/main/java/com/diviso/graeshoppe/config/SocketIOServerConfiguration.java
+++ src/main/java/com/diviso/graeshoppe/config/SocketIOServerConfiguration.java
@@ -19,6 +19,7 @@ public class SocketIOServerConfiguration {
 	
 	@Bean
 	public SocketIOServer getSocketIoServer() {
+		System.out.println("Host is "+host+" port is "+port);
 		com.corundumstudio.socketio.Configuration configuration = new com.corundumstudio.socketio.Configuration();
 		configuration.setHostname(host);
 		configuration.setPort(port);
