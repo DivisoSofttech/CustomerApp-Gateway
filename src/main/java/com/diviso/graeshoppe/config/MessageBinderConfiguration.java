9ce9b86320261d00da5eea51e7ccdd5dc14689d1
abilash-diviso <abilash.s@lxisoft.com>
2019-09-24 14:39:04 +0530

    notification package name has been changed

--- src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
+++ src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
@@ -3,7 +3,7 @@ package com.diviso.graeshoppe.config;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.Input;
 
-import com.diviso.graeshoppe.order.avro.Notificaton;
+import com.diviso.graeshoppe.notification.avro.Notificaton;
 
 
 public interface MessageBinderConfiguration {
