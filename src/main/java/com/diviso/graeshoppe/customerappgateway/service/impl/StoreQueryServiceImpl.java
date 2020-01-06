7c4bbd8f48aa0591ded62b8931df4813e1354409
rafeekbytatech <abdul.rafeek@lxisoft.com>
2020-01-06 15:03:17 +0530

    refactored suggestion method

--- src/main/java/com/diviso/graeshoppe/customerappgateway/service/impl/StoreQueryServiceImpl.java
+++ src/main/java/com/diviso/graeshoppe/customerappgateway/service/impl/StoreQueryServiceImpl.java
@@ -620,37 +620,42 @@ public class StoreQueryServiceImpl implements StoreQueryService {
 			if (hit.getIndex().equals("store")) {
 				//Store store=objectMapper.convertValue(hit.getSourceAsMap(),new Store());
 				System.out.println("zZZZZZZZZZZZZZZZZZZstore"+hit.getIndex());
-				System.out.println("zZZZZZZZZZZZZZZZZZZstore"+sourceAsMap.get("id"));
+				System.out.println("zZZZZZZZZZZZZZZZZZZstore"+(long)sourceAsMap.get("id"));
 				System.out.println("zZZZZZZZZZZZZZZZZZZstore"+(String) sourceAsMap.get("name"));
 				System.out.println("zZZZZZZZZZZZZZZZZZZstore"+(String) sourceAsMap.get("imageLink"));
+				Store store=objectMapper.convertValue(hit.getSourceAsMap(),Store.class);
+				
 				long id=(long)sourceAsMap.get("id");
 				Long d= id;
 				result.setResultType(hit.getIndex());
-				result.setId(d);
-				result.setName((String) sourceAsMap.get("name"));
-				result.setImageLink((String) sourceAsMap.get("imageLink"));
+				result.setId(store.getId());
+				result.setName(store.getName());
+				result.setImageLink(store.getImageLink());
 
 			} else if (hit.getIndex().equals("product")) {
 				System.out.println("zZZZZZZZZZZZZZZZZZZproduct"+hit.getIndex());
 				System.out.println("zZZZZZZZZZZZZZZZZZZproduct"+ sourceAsMap.get("id"));
 				System.out.println("zZZZZZZZZZZZZZZZZZZproduct"+(String) sourceAsMap.get("name"));
 				System.out.println("zZZZZZZZZZZZZZZZZZZv"+(String) sourceAsMap.get("imageLink"));
-				Long id=(Long)sourceAsMap.get("id");
+				com.diviso.graeshoppe.customerappgateway.client.product.model.Product product=objectMapper.convertValue(hit.getSourceAsMap(),com.diviso.graeshoppe.customerappgateway.client.product.model.Product.class);
+				
+				
 				result.setResultType(hit.getIndex());
-				result.setId(id);
-				result.setName((String) sourceAsMap.get("name"));
-				result.setImageLink((String) sourceAsMap.get("imageLink"));
+				result.setId(product.getId());
+				result.setName(product.getName());
+				result.setImageLink(product.getImageLink());
 
 			} else if (hit.getIndex().equals("category")) {
 				System.out.println("zZZZZZZZZZZZZZZZZZZcategory"+hit.getIndex());
 				System.out.println("zZZZZZZZZZZZZZZZZZZcategory"+ sourceAsMap.get("id"));
 				System.out.println("zZZZZZZZZZZZZZZZZZZcategory"+(String) sourceAsMap.get("name"));
 				System.out.println("zZZZZZZZZZZZZZZZZZZcategory"+(String) sourceAsMap.get("imageLink"));
+				com.diviso.graeshoppe.customerappgateway.client.product.model.Category category =objectMapper.convertValue(hit.getSourceAsMap(),com.diviso.graeshoppe.customerappgateway.client.product.model.Category.class);
 				result.setResultType(hit.getIndex());
-				Long id=(Long)sourceAsMap.get("id");
-				result.setId(id);
-				result.setName((String) sourceAsMap.get("name"));
-				result.setImageLink((String) sourceAsMap.get("imageLink"));
+				
+				result.setId(category.getId());
+				result.setName(category.getName());
+				result.setImageLink(category.getImageLink());
 
 			}
 
@@ -668,7 +673,8 @@ public class StoreQueryServiceImpl implements StoreQueryService {
 
 		for (SearchHit hit : searchHit) {
 			//System.out.println("............T............"+t);
-			list.add((T)objectMapper.convertValue(hit.getSourceAsMap(), t.getClass()));
+		T t1=	(T)objectMapper.convertValue(hit.getSourceAsMap(), t.getClass());
+	Store store=objectMapper.convertValue(hit.getSourceAsMap(),Store.class);
 		}
 
 		return new PageImpl(list, page, response.getHits().getTotalHits());
